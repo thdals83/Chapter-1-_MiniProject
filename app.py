ef05f38c59355d2174dd489a1ef9420ac20fc62a
thdals83 <thdals83@naver.com>
2022-01-10 17:24:41 +0900

    1

---
+++ app.py
@@ -0,0 +1,23 @@
+import hashlib
+
+from bson.json_util import dumps
+from operator import itemgetter
+import jwt
+from bson import ObjectId
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.card
+
+SECRET_KEY = 'SPARTA'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
