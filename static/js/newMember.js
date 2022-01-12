3edfc1d5f73dd66e375c2cc1afd61c4905eca2a1
Yang-Seung-Hun <simononze@naver.com>
2022-01-12 14:39:22 +0900

    세부 사항

--- static/js/newMember.js
+++ static/js/newMember.js
@@ -86,15 +86,20 @@
             }
         }
 
-            // $(function () {
-            //     $("#direct").hide();
-            //     $("#email2").change(function () {
-            //         if ($("#email2").val() == "direct") {
-            //             $("#direct").show();
-            //         } else {
-            //             $("#email2").hide();
-            //         }
-            //     })
-            // });
+            function email_change() {
+                if (document.join.email.options[document.join.email.selectedIndex].value == '0') {
+                    document.join.email2.disabled = true;
+                    document.join.email2.value = "";
+                }
+                if (document.join.email.options[document.join.email.selectedIndex].value == '9') {
+                    document.join.email2.disabled = false;
+                    document.join.email2.value = "";
+                    document.join.email2.focus();
+                } else {
+                    document.join.email2.disabled = true;
+                    document.join.email2.value = document.join.email.options[document.join.email.selectedIndex].value;
+                }
+            }
+
 
 
